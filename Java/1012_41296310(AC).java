d8acdf849884c612eed88bc56b1b64e0e261fff6
kmbae <102423525+kmbae1142@users.noreply.github.com>
2025-02-05 20:25:57 +0900

    Add files via upload

---
+++ Java/1012_41296310(AC).java
@@ -0,0 +1,9 @@
+﻿import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		float f = sc.nextFloat();
+		System.out.printf("%.6f", f);
+	}
+}
